064bbd0f4f2adbd521de5f45731a2be2dd87bd7c
John <johnmulvey48@yahoo.com>
2022-05-02 10:10:56 -0400

    commit tree b1f71bf3156d775e182da9b8e34136ab2ab312ff

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-function fetchBooks(_fetch("https://anapioficeandfire.com/api/books")) {
-  .then((response) => response.json())
+function fetchBooks() {
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
   .then((json) => console.log(json));
 }
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks(renderBooks);
-});
-
-return books;
